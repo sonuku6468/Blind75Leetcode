bf12adefedfe61b92472038eb5d70d8a0da0c6fb
sonuku6468 <108977415+sonuku6468@users.noreply.github.com>
2023-12-28 21:15:19 +0530

    third commit

---
+++ Arrays/duplicate.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+//git commit -m "Your commit message"
+using namespace std;
+
+int duplicate(vector<int>& element) {
+    int cnt=0;
+    for(int i=0;i<element.size();i++)
+    {
+        for(int j=i+1;j<element.size();j++)
+        {
+            if(element[i]==element[j])
+            {
+               cnt++;
+            }
+            
+        }
+       
+        
+    }
+     if(cnt>=1)
+        {
+            return 1;
+        }
+    return 0;
+    cout<<cnt;
+}
+
+int main() {
+    vector<int> element = {1,2,3,1};
+    cout << "duplicate element: " << duplicate(element) << "\n";
+    return 0;
+}
