0778f6cf6bbf4053b828e90c17bd049346b9c169
sonuku6468 <108977415+sonuku6468@users.noreply.github.com>
2024-03-15 08:34:12 +0530

    commit

--- tree/max_depth.cpp
+++ tree/max_depth.cpp
@@ -1,6 +1,10 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    return 0;
-}
+
+ int maxDepth(TreeNode* root) {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+        return 1+max(lh,rh);
+ }
